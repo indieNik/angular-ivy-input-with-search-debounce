618269e2db11c7ce464c6c79687b58bf28aba881
indieNik <niki.thrill@gmail.com>
2021-11-08 21:40:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-input-with-search
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-input-with-search)
---
+++ src/app/search-input/search-input.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
+import names from './names.model';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.css'],
+})
+export class SearchInputComponent implements OnInit, OnDestroy {
+  searchItems: string[];
+  searchText$ = new Subject<String>();
+  _searchSub: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.loadNames();
+    this._searchSub = this.searchText$
+      .pipe(debounceTime(700))
+      .subscribe((str) => {
+        if (str.trim().length > 2) {
+          // console.log('Search string: ', str);
+          this.updateSearchResults(str);
+        } else {
+          this.resetInput();
+        }
+      });
+  }
+
+  loadNames() {
+    this.searchItems = [...names];
+    // console.log(`this.searchItems: ${this.searchItems}`);
+  }
+
+  trackInputChanges(e) {
+    this.searchText$.next(e.target.value);
+  }
+
+  updateSearchResults(searchStr) {
+    // console.log(`searchStr: ${searchStr}`);
+    this.searchItems = [...names].filter((name) => {
+      return name.toLowerCase().includes(searchStr.toLowerCase());
+    });
+    // console.log(`Filtered searchItems: ${this.searchItems}`);
+  }
+
+  resetInput() {
+    this.searchItems = [];
+  }
+
+  ngOnDestroy() {
+    this._searchSub.unsubscribe();
+  }
+}
